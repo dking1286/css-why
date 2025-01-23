98b1b2a1d849843ec62b0117c25d95af124d4379
Daniel O King <daniel.oliver.king@gmail.com>
2025-01-23 11:48:54 -0800

    Move some code into a lib.

--- bin/frontend/Main.ml
+++ bin/frontend/Main.ml
@@ -1,7 +1,2 @@
-open Webapi.Dom
-
 let () = Js.log "Hello World!"
-
-let () =
-  let root = document |> Document.getElementById "root" in
-  match root with None -> () | Some node -> Element.setInnerText node "foo"
+let () = Frontend.Root.showTextInRoot "foo"
